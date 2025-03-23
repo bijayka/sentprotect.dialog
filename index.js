4260d41e5e4db3a14877be4b595c2ca222977834
bijayka <bijaykapali@gmail.com>
2025-03-23 21:51:50 +0000

    test

--- index.js
+++ index.js
@@ -4,16 +4,14 @@
 */
 w_indexjs_globa_var = "Hello World!";
 var extRecipients = [];
+var extAttachments = [];
 let item;
 
 // Confirms that the Office.js library is loaded.
 Office.onReady((info) => {
   if (info.host === Office.HostType.Outlook) {
       item = Office.context.mailbox.item;
-      getAllRecipients();
-      sampleFunction();
-      console.log('extRecipients');
-      console.log(extRecipients); 
+      console.log('Office is ready');
   }
 });
 
@@ -91,8 +89,7 @@ function addAddresses (recipients) {
 
   function onMessageSendHandler(event) {
     console.warn(w_indexjs_globa_var);
-    //getBcc();
-    //Office.context.mailbox.item.to.getAsync({ asyncContext: event }, getRecipientsCallback);
+    Office.context.mailbox.item.to.getAsync({ asyncContext: event }, getToRecipientsCallback);
   }
 
   function myAttachments() {
@@ -155,6 +152,132 @@ function addAddresses (recipients) {
     });
   }
 
+  function getToRecipientsCallback(asyncResult) {
+    const event = asyncResult.asyncContext;
+    if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+      const message = "Failed to get to recipients";
+      console.error(message);
+      event.completed({ allowEvent: false, errorMessage: message });
+      return;
+    }
+
+    const recipients = asyncResult.value;
+    externalRecipients = recipients.filter(recipient => {
+      const email = recipient.emailAddress.toLowerCase();
+      return !email.endsWith("@ey.com") && !email.endsWith("@ey.net");
+    });
+
+    if (externalRecipients.length > 0) {
+      externalRecipients.forEach((recipient, index) => {
+        console.log(`External recipient ${index + 1}: ${recipient.emailAddress}`); 
+        extRecipients.push(recipient.emailAddress); 
+
+      });
+    } 
+    item.cc.getAsync({ asyncContext: event }, getCCRecipientsCallback);
+  }
+
+  function getCCRecipientsCallback(asyncResult) {
+    const event = asyncResult.asyncContext;
+    if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+      const message = "Failed to get CC recipients";
+      console.error(message);
+      event.completed({ allowEvent: false, errorMessage: message });
+      return;
+    }
+
+    const recipients = asyncResult.value;
+    externalRecipients = recipients.filter(recipient => {
+      const email = recipient.emailAddress.toLowerCase();
+      return !email.endsWith("@ey.com") && !email.endsWith("@ey.net");
+    });
+
+    if (externalRecipients.length > 0) {
+      externalRecipients.forEach((recipient, index) => {
+        console.log(`External recipient ${index + 1}: ${recipient.emailAddress}`); 
+        extRecipients.push(recipient.emailAddress); 
+
+      });
+    } 
+    item.bcc.getAsync({ asyncContext: event }, getBccRecipientsCallback);
+  }
+
+  function getBccRecipientsCallback(asyncResult) {
+    const event = asyncResult.asyncContext;
+    if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+      const message = "Failed to get BCC recipients";
+      console.error(message);
+      event.completed({ allowEvent: false, errorMessage: message });
+      return;
+    }
+
+    const recipients = asyncResult.value;
+    externalRecipients = recipients.filter(recipient => {
+      const email = recipient.emailAddress.toLowerCase();
+      return !email.endsWith("@ey.com") && !email.endsWith("@ey.net");
+    });
+
+    if (externalRecipients.length > 0) {
+      externalRecipients.forEach((recipient, index) => {
+        console.log(`External recipient ${index + 1}: ${recipient.emailAddress}`); 
+        extRecipients.push(recipient.emailAddress); 
+
+      });
+    } 
+    if (extRecipients.length > 0) {
+      item.getAttachmentsAsync({ asyncContext: event }, getAttachmentsCallback2);
+    } else {
+      event.completed({ allowEvent: true });
+    }
+  }
+
+  function getAttachmentsCallback2(asyncResult) {
+    const { event } = asyncResult.asyncContext;
+    if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
+      const message = "Failed to retrieve attachments. Please try again or contact support.";
+      console.error(message);
+      event.completed({ allowEvent: false, errorMessage: message });
+      return;
+    }
+
+    const attachments = asyncResult.value;
+
+    if (!attachments || attachments.length === 0) {
+      event.completed({ allowEvent: true });
+      return;
+    }
+
+    const nonImageAttachments = attachments.filter(attachment => {
+      if (!attachment.name) return false; // Skip attachments without a name
+      const extension = attachment.name.split('.').pop().toLowerCase();
+      const imageExtensions = ["gif", "jpg", "png", "webp", "tif", "tiff", "jpeg", "jif", "jfif", "jp2", "jpx", "j2k", "j2c"];
+      return !imageExtensions.includes(extension);
+    });
+
+    if (nonImageAttachments.length > 0) {
+      const externalEmails = externalRecipients.map(recipient => recipient.emailAddress).join("\n- ");
+      const attachmentNames = nonImageAttachments.map(attachment => attachment.name).join("\n- ");
+      const message = `## External Recipients
+A list of external email addresses with checkboxes:
+- ${externalEmails}
+
+## Attachments
+A list of file attachments with checkboxes:
+- ${attachmentNames}
+      `;
+      nonImageAttachments.forEach((mAttachmnt, index) => {
+        console.log(`Attachment ${index + 1}: ${mAttachmnt.name}`); 
+        extAttachments.push(mAttachmnt.name); 
+
+      });
+      //event.completed({ allowEvent: false, errorMessage: message, sendModeOverride: Office.MailboxEnums.SendModeOverride.PromptUser });
+      event.completed({ allowEvent: false, errorMessage: "Your email includes external recipients with attachment; please review it before sending.", commandId: "msgComposeOpenPaneButton" });
+    } else {
+      event.completed({ allowEvent: true });
+    }
+  }
+
+
   function getRecipientsCallback(asyncResult) {
     const event = asyncResult.asyncContext;
     if (asyncResult.status !== Office.AsyncResultStatus.Succeeded) {
@@ -164,7 +287,7 @@ function addAddresses (recipients) {
       return;
     }
 
-    const recipients = asyncResult.value;
+    const recipients = asyncResult.to.value .value;
     externalRecipients = recipients.filter(recipient => {
       const email = recipient.emailAddress.toLowerCase();
       return !email.endsWith("@ey.com") && !email.endsWith("@ey.net");
