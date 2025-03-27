bb747aee4caa92be02ee48b9dfe934349caac9e1
tejamogadala <mogadalateja2002@gmail.com>
2025-03-27 12:25:53 +0530

    n

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ Office.onReady((info) => {
 });
 
 
+
   function onMessageSendHandler(event) {
     console.warn(w_indexjs_globa_var);
     Office.context.mailbox.item.to.getAsync({ asyncContext: event }, getToRecipientsCallback);
